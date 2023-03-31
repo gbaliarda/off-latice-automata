352f95b74e518a9dfa1a9458907a4d75d577564d
Ezequiel <eperez@itba.edu.ar>
2023-03-31 13:51:01 -0300

    chore: start va_time_mul from step 1 instead of 0

--- visualization/va_time_mul.py
+++ visualization/va_time_mul.py
@@ -4,12 +4,12 @@ from parsers import ITERATIONS
 
 # JUMP TO LINE 67 TO SEE THE MAIN FUNCTION
 
-def compute_va_time(N: int, steps: dict[int, dict[int, list[float]]]):
+def compute_va_time(N: int, t_0: int, steps: dict[int, dict[int, list[float]]]):
   # v_a for each time step
   v_a = np.zeros(ITERATIONS)
 
   # Compute v_a for each time step
-  for i in range(ITERATIONS):
+  for i in range(t_0, ITERATIONS):
     # Sum the velocities of all particles as a vector
     V = np.array([0.0, 0.0])
     for j in range(N):
@@ -22,24 +22,6 @@ def compute_va_time(N: int, steps: dict[int, dict[int, list[float]]]):
     v_a[i] = (1 / (N*v)) * V
 
   return v_a
-
-
-def parse_static_inputs(file_path: str):
-  # Save the particles positions, velocity and angle at each time step
-  steps: dict[int, dict[int, list[float]]] = {}
-  steps[0] = {}
-
-  with open(file_path, "r") as f:
-    f.readline().rstrip("\n") # skip the interaction radius line
-
-    index = 0
-    # Parse initial particle positions, velocity and angle
-    for line in f:
-      # skip the particle radius
-      steps[0][index] = [float(x) for x in line.rstrip("\n").split(" ")][1:]
-      index += 1
-    
-    return steps
 
 
 def parse_simulation_output(N, steps: dict[int, dict[int, list[float]]], file_path: str):
@@ -72,23 +54,22 @@ EXPERIMENTS = 2
 
 # List of experiments to run
 parameters = {
-  "static_files": ["static_400_10.txt", "static_100_5.txt"],
   "output_files": ["output_400_10.txt", "output_100_5.txt"],
   "N": [400, 100],
   "labels": ["N=400, L=10, η=1", "N=100, L=5, η=1"]
 }
 
-for i in range(EXPERIMENTS):
-  # Parse the static inputs
-  steps = parse_static_inputs(parameters["static_files"][i])
+# Save the particles positions, velocity and angle at each time step
+steps: dict[int, dict[int, list[float]]] = {}
 
+for i in range(EXPERIMENTS):
   N = parameters["N"][i]
 
   # Parse the simulation output
   parse_simulation_output(N, steps, parameters["output_files"][i])
 
   # Compute v_a for each time step
-  v_a = compute_va_time(N, steps)
+  v_a = compute_va_time(N, 1, steps)
 
   plt.plot(t, v_a, label=parameters["labels"][i])
 
